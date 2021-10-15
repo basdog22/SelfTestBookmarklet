e7d0a507b9357b5d2ef4d8c79daabe8a75d26cdf
Vasilis Kerasiotis <basdog22@gmail.com>
2021-10-15 16:02:29 +0300

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,11 +1,19 @@
-javascript:(function() {
-    if(typeof window.setNativeValue == 'undefined'){
-
+javascript: (function() {
+    let details = {
+        'child_firstname':        'Little',
+        'child_surname':          'Doe',
+        'child_fathername':       'John',
+        'child_mothername':       'Jane',
+        'child_birth_date-day':   '01',
+        'child_birth_date-month': '01',
+        'child_birth_date-year':  '2010',
+        'amka':                   '01011099999'
+    };
+    if (typeof window.setNativeValue == 'undefined') {
         function setNativeValue(element, value) {
             const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
             const prototype = Object.getPrototypeOf(element);
             const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
-
             if (valueSetter && valueSetter !== prototypeValueSetter) {
                 prototypeValueSetter.call(element, value);
             } else {
@@ -13,37 +21,17 @@ javascript:(function() {
             }
         }
     }
-    var today = new Date();
-
-
-    var elements_1 = {
-        "child_firstname": "John", /*//Kid first name*/
-        "child_surname": "Doe", /*//Kid last name*/
-        "child_fathername": "Jack", /*//Father first name*/
-        "child_mothername": "Mary", /*//Mother first name*/
-        "child_birth_date-day": "23", /*// Birth day*/
-        "child_birth_date-month": "06",/*// Birth month*/
-        "child_birth_date-year": "2014", /*//Birth year*/
-    };
-
-    var elements_2 = {
-        "amka": "23061400000", /*//Kid amka*/
-        "test_date-day": String(today.getDate()).padStart(2, '0'),
-        "test_date-month": String(today.getMonth() + 1).padStart(2, '0'),
-        "test_date-year": today.getFullYear(),
-    };
-
-    var finel;
-
-    if(document.querySelector("[name='child_firstname']")){
-        finel = elements_1;
-    }else{
-        finel = elements_2;
-    }
-
-    for (const [key, value] of Object.entries(finel)) {
-        var input = document.querySelector("[name='"+key+"']");
-        setNativeValue(input, value);
-        input.dispatchEvent(new Event('input', { bubbles: true }));
+    const today = new Date();
+    details['test_date-day'] = String(today.getDate()).padStart(2, '0');
+    details['test_date-month'] = String(today.getMonth() + 1).padStart(2, '0');
+    details['test_date-year'] = today.getFullYear();
+    for (const [key, value] of Object.entries(details)) {
+        if (document.querySelector("[name='"+key+"']")) {
+            var input = document.querySelector("[name='"+key+"']");
+            setNativeValue(input, value);
+            input.dispatchEvent(new Event('input', {
+                bubbles: true
+            }));
+        }
     }
 })();
